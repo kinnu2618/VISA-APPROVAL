215d31f0bc1414a3796279bf2a77176abfa39c21
kinnu2618 <sunnrayss9@gmail.com>
2024-08-15 22:11:08 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "VISA-APPROVAL",
+    version = "0.0.0",
+    author = "kinnu2618",
+    author_email = "sunnrayss9@gmail.com",
+    packages = find_packages()
+)
