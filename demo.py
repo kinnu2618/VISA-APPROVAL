de9db10e722da31b91f933a1b477116d06110b54
kinnu2618 <sunnrayss9@gmail.com>
2024-08-16 13:13:39 +0530

    utils added

--- demo.py
+++ demo.py
@@ -1,3 +1,12 @@
 from visa.logger import logging
+from visa.exception import visaException
+import sys
+
 
 logging.info("hi")
+
+
+try:
+    a = 2/0
+except Exception as e:
+    raise visaException(e, sys)
